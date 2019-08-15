a7f08aad4d4efdf1e28c4d17c078a93cefd1e4dd
thalysonizzatx2 <thalyson.gabriel.a.sousa@gmail.com>
2019-08-14 21:30:16 -0300

    Upp

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import prof from '../../imgs/icons8-b-50.png'
 
 import './styles.css'
 
-export default function
-  () {
-
+export default function Board() {
+  const [name, setName] = useState('')
   const names = ["Thalyson Sousa", "Natasha Romanoff", "Loki"]
   return (
     <div className="board">
@@ -16,9 +15,9 @@ export default function
         </div>
         {
           names.map(name => (
-            <div className="conts-c">
+            <div className="conts-c" onClick={() => setName(name)}>
               <div className="img">
-                <img src="https://img.icons8.com/ios-glyphs/30/000000/cat-profile--v2.png" />
+                <img src="https://img.icons8.com/ios-glyphs/30/000000/cat-profile--v2.png" alt="icon" />
               </div>
               <div className="name">
                 <span>{name}</span>
@@ -31,17 +30,27 @@ export default function
         }
       </div>
       <div className="chat-b">
+
         <div className="title-chat">
           <div className="name-chat">
             <div className="img">
-              <img src="https://img.icons8.com/ios-glyphs/30/000000/cat-profile--v2.png" />
+              <img src="https://img.icons8.com/ios-glyphs/30/000000/cat-profile--v2.png" alt="icon" />
             </div>
             <div className="name">
-              <span>Thalyson Sousa</span>
+              <span>{name}</span>
             </div>
           </div>
         </div>
+        <div className="msgm-chat">
+
+        </div>
+
+        <div className="set-chat">
+          <input type="text" placeholder="Mensagem" name="msgm" />
+          <button>>>></button>
+        </div>
       </div>
+
     </div>
   );
 }
